b2f53e3b24861394935b871d46778d8c39a86f9f
Denis <felon_91@mail.ru>
2020-04-21 11:16:41 +0300

    initial project  - added routes  - added posts component  - added error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PostsComponent} from './posts/posts.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PostsComponent},
+  {path: 'error', component: ErrorPageComponent},
+  {path: '**', redirectTo: '/error'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
